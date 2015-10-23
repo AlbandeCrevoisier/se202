091f9caa634346988b9a047862493363ce6b11e6
ACH <albandecrevoisier@gmail.com>
2015-10-23 13:10:21 +0200

    Added proper dumper messages for functions and let-in-end.

--- parser/dumper.py
+++ parser/dumper.py
@@ -31,10 +31,12 @@ class Dumper(Visitor):
         ret_str = "let "
         if (len(let.decls) != 0):
             for decl in let.decls:
-                ret_str += decl.accept(self)
-        ret_str += " in "
+                ret_str += decl.accept(self) + " "
+        ret_str += "in "
         for exp in let.exps:
             ret_str += exp.accept(self)
+            if (exp != let.exps[-1]):
+                ret_str += ", "
         ret_str += " end"
         return ret_str
 
@@ -75,11 +77,17 @@ class Dumper(Visitor):
     @visitor(FunDecl)
     def visit(self, fdecl):
         ret_str = "function %s(" % fdecl.name
-        if (len(fdecl.args) != 0):
+        if (len(fdecl.args) == 1):
             for arg in fdecl.args:
-                ret_str += arg.accept(self)
+                ret_str += "%s : %s" % (arg.name, arg.type.accept(self))
+        elif (len(fdecl.args) != 0):
+            for arg in fdecl.args:
+                ret_str += "%s : %s" % (arg.name, arg.type.accept(self))
+                if (arg != fdecl.args[-1]):
+                    ret_str += ", "
         ret_str += ") "
         if (fdecl.type != None):
             ret_str += ": " + fdecl.type.accept(self) + " "
-        ret_str += fdecl.exp.accept(self)
+        ret_str += "= "
+        ret_str += fdecl.exp.accept(self)[1:-1]
         return ret_str
